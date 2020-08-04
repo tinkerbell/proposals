ef524e4f2c89c9dad1858290de804b09732bc4ae
Gianluca Arbezzano <gianarb92@gmail.com>
2020-08-04 08:45:12 +0200

    0002: Adding placeholder for RFD "Release"
    Signed-off-by: Gianluca Arbezzano <gianarb92@gmail.com>

--- scripts/new.sh
+++ scripts/new.sh
@@ -2,17 +2,17 @@
 
 set -eu
 
-RFD_ID=$1
-RFD_TITLE=$2
+ID=$1
+TITLE=$2
 
-echo "🐙 >>> started the creation process for RFD ${RFD_ID} titled \"${RFD_TITLE}\""
+echo "🐙 >>> started the creation process for RFD ${ID} titled \"${TITLE}\""
 
-branch_exists=$(git branch -rl *${RFD_ID})
+branch_exists=$(git branch -rl *${ID})
 
 if [[ -z ${branch_exists} ]]; then
-    git checkout -b ${RFD_ID}
-    mkdir $RFD_ID
-    cp ../prototypes/prototype.md $RFD_ID/README.md
+    git checkout -b ${ID}
+    mkdir $ID
+    cat ./prototypes/prototype.md | sed s/ID/$ID/ | sed s/TITLE/"$TITLE"/ > $ID/README.md
     echo "🍀 >>> You are all set! Good luck"
     exit 0
 else
